9ac11910075e82a29f4e6f98f4746df027fee845
Murillo Teixeira <murillo.teixeira@usp.br>
2023-05-11 10:55:57 +0100

    Creating project and listener

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/murillo/monte-carlo-localization/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/murillo/testing_ros/devel;/home/murillo/tut_ws/devel;/opt/ros/noetic'.split(';') if '/home/murillo/testing_ros/devel;/home/murillo/tut_ws/devel;/opt/ros/noetic' != '' else []
---
+++ src/reading_odom/scripts/read_odometry.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+
+import rospy
+from nav_msgs.msg import Odometry
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+
+if __name__ == '__main__':
+    rospy.init_node("robot_controller")
+
+    rospy.Subscriber("/pose", Odometry, callback)
+    
+    rospy.spin()
+    rospy.loginfo("Closing subscriber")
