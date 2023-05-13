725e346c7c919a285c31a05791fe321807ab6020
Murillo Teixeira <murillo.teixeira@usp.br>
2023-05-13 12:30:59 -0700

    Creating new build process

---
+++ src/demo_ros/src/demo_python/bayes_filter.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+class BayesFilter:
+    """
+    Do you implementation of the BayesFilter here
+    """
+
+    def __init__(self):
+
+        # Initiate with the initial state of the vehicle
+        # composed of x=[x, y, z, x_dot, y_dot, z_dot, yaw, yaw_dot]
+
+        # TODO
+        pass
+
+    def predict(self, dt, u):
+        
+        # TODO
+        print('Predicting')
+
+    def update(self, z):
+
+        # TODO
+        print('Updating')
