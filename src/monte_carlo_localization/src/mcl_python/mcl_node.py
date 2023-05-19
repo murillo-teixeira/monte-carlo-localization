c979aac10bee12200c55464af05848646651f00c
Murillo Teixeira <murillo.teixeira@usp.br>
2023-05-19 19:40:18 +0100

    Loading and plotting map on python

--- src/monte_carlo_localization/src/mcl_python/mcl_node.py
+++ src/monte_carlo_localization/src/mcl_python/mcl_node.py
@@ -1,33 +1,60 @@
 #!/usr/bin/env python3
 
 import rospy
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+
+from nav_msgs.msg import Odometry, OccupancyGrid
 from std_msgs.msg import Float64
-from nav_msgs.msg import Odometry
 
 from classes.Particle import Particle
 
 class MonteCarloLocalizationNode:
 
     def __init__(self):
+        
+        rospy.init_node('monte_carlo_localization')
+        rospy.loginfo_once('MCL has started')
 
-        # Initialize some necessary variables here
-        self.node_frequency = None
-        self.sub_fake_sensor_topic = None
-        self.pub_demo_topic = None
+        self.map_file       = rospy.get_param("~map_file")
+        self.node_frequency = rospy.get_param("node_frequency", 1)
+        self.map_xmin       = rospy.get_param("map_xmin", 0)
+        self.map_xmax       = rospy.get_param("map_xmax", 0)
+        self.map_ymin       = rospy.get_param("map_ymin", 0)
+        self.map_ymax       = rospy.get_param("map_ymax", 0)
+        
+        self.map_matrix = self.load_map()
+        self.plot_map()
+
         
         self.particles = None
 
         # Initialize the ROS node
-        rospy.init_node('demo_node')
-        rospy.loginfo_once('Demo Node has started')
-        self._initialize_particles(10)
+        self.initialize_particles(10)
 
-    def _initialize_particles(self, number_of_particles = 50):
+    def load_map(self):
+        with open(self.map_file, 'rb') as f:
+            lines = f.readlines()
+            width, height = np.array(lines[2].split(), dtype=int)
+            map_array = np.array([el for el in lines[4]], dtype=np.int16)
+            # print('unique: ', np.unique(map_array))
+            map_matrix = np.array(map_array, dtype=float).reshape(height, width)
+            map_matrix[map_matrix==205] = 0.5
+            map_matrix[map_matrix==0] = 0
+            map_matrix[map_matrix==254] = 1
+        
+        return map_matrix
+
+    def plot_map(self):
+        plt.imshow(self.map_matrix[self.map_ymin:self.map_ymax, self.map_xmin:self.map_xmax], cmap='gray')
+        plt.show()
+
+    def initialize_particles(self, number_of_particles = 50):
         self.particles = []
         for _ in range(number_of_particles) :
             self.particles.append(Particle(0, 0, 0))
-        rospy.loginfo_once(self.particles)
-
+        # rospy.loginfo_once(self.particles)
 
 def main():
 
