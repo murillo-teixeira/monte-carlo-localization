f2c3d829601f5a87cf0d58c0e6f5a05ea6487045
PedroJRBernardes <pjrbernardes@gmail.com>
2023-05-29 13:22:55 +0100

    Correction of the resampler_simul

--- src/mcl_python/scripts/resampler_simulation.py
+++ src/mcl_python/scripts/resampler_simulation.py
@@ -11,7 +11,7 @@ def resampler(particles, weights):
 
     for m in range(number_of_particles):
         u = r + (m - 1)/number_of_particles
-        while u > c:
+        while u > c:        #in this cycle we find the best place in our new particle set for the particle m
             i += 1
             c += weights[i-1]
         
@@ -29,19 +29,19 @@ fig, ax = plt.subplots(figsize=(8, 6))
 # Plot the initial particles with transparency
 x_initial = [particle[0] for particle in particles]
 y_initial = [particle[1] for particle in particles]
-ax.scatter(x_initial, y_initial, color='blue', marker='o', alpha=0.2, label='Initial')
+ax.scatter(x_initial, y_initial, color='blue', marker='o', alpha=0.2, s=100, label='Initial')
 
 # Define colormap and marker styles
 cmap = get_cmap('tab10')
 markers = ['s', 'o', 'D', 'v', '^']
-num_iterations = 5
+num_iterations = 4
 
 for iteration in range(num_iterations):
     print(weights)
     resampled_particles = resampler(particles, weights)
     particles = resampled_particles
     if iteration == 1:
-        weights = [0.05, 0.15, 0.6, 0.05, 0.15]
+        weights = [0.05, 0.1, 0.65, 0.05, 0.15]
     elif iteration == 2:
         weights = [0, 0.1, 0.8, 0, 0.1]
 
@@ -49,7 +49,7 @@ for iteration in range(num_iterations):
     marker = markers[iteration]
     x_resampled = [particle[0] for particle in resampled_particles]
     y_resampled = [particle[1] for particle in resampled_particles]
-    ax.scatter(x_resampled, y_resampled,marker=marker, alpha=0.7, label='Iteration {}'.format(iteration + 1))
+    ax.scatter(x_resampled, y_resampled,marker=marker, alpha=0.7,s=100, label='Iteration {}'.format(iteration + 1))
     
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
